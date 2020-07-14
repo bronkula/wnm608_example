30e9a15331cfbbb859ead0e3e488db2c820060a1
Hamilton <hamdiggy@gmail.com>
2020-07-14 07:30:58 -0700

    m07 php math

--- cline.hamilton/notes/index.php
+++ cline.hamilton/notes/index.php
@@ -28,7 +28,48 @@
 
 
 
+	// Number
+	// Integer
+	$b = 15;
+	// Float
+	$b = 0.576;
+
+	$b = 10;
+
+
+	// String
+	$name = "Yerguy2";
+
+	// Boolean
+	$isOn = true;
+
+
+
+
+
+	// Math
+	// PEMDAS
+	echo (5 - 4) * 2;
+
+	// Concatenation
+	echo "<div>b + a" . " = c</div>";
+	echo "<div>$b + $a = ".($a+$b)."</div>";
 
 	?>
+
+	<hr>
+	<div>This is my name</div>
+	<div>
+		
+	<?php
+
+	$firstname
+	$lastname
+	$fullname
+
+	echo $fullname;
+
+	?>
+
 </body>
 </html>
