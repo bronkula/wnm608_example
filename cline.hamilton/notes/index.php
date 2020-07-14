57cebbf1a77d78e1c41e18e98d265c0be17d7067
Hamilton <hamdiggy@gmail.com>
2020-07-14 05:36:02 -0700

    m07 php variables

---
+++ cline.hamilton/notes/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+
+
+
+
+
+	echo "<div> Hello World </div>";
+	echo "<div> Goodbye World </div>";
+
+	// Variables
+	$a = 5;
+
+	echo $a;
+
+
+	// String Interpolation
+	echo "<div>I have $a things</div>";
+	echo '<div>I have $a things</div>';
+
+
+
+
+
+
+	?>
+</body>
+</html>
