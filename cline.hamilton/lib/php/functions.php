a1c0594fb4b3caf7a7e8e0f6e10314d8e3093afc
Hamilton <hamdiggy@gmail.com>
2020-07-20 06:09:41 -0700

    m08 looping data

--- cline.hamilton/lib/php/functions.php
+++ cline.hamilton/lib/php/functions.php
@@ -4,3 +4,9 @@
 function print_p($v) {
 	echo "<pre>",print_r($v),"</pre>";
 }
+
+
+function file_get_json($filename) {
+	$file = file_get_contents($filename);
+	return json_decode($file);
+}
--- cline.hamilton/notes/reading_data.php
+++ cline.hamilton/notes/reading_data.php
@@ -2,11 +2,10 @@
 
 include "../lib/php/functions.php";
 
-$filename = "notes.json";
-$file = file_get_contents($filename);
-$notes = json_decode($file);
+$notes_object = file_get_json("notes.json");
+$users_array = file_get_json("../data/users.json");
 
-print_p($notes);
+// print_p($notes);
 
 ?>
 <!DOCTYPE html>
@@ -25,7 +24,28 @@ print_p($notes);
 		<div class="card soft">
 			<h2>Notes</h2>
 
+			<?php
 
+			for($i=0;$i<count($notes_object->notes);$i++){
+				echo "<li>{$notes_object->notes[$i]}</li>";
+			}
+
+			?>
+		</div>
+
+		<div class="card soft">
+			<h2>Users</h2>
+
+			<?php
+
+			for($i=0;$i<count($users_array);$i++){
+				echo "<li>
+					<strong>{$users_array[$i]->name}</strong>
+					<span>{$users_array[$i]->type}</span>
+				</li>";
+			}
+
+			?>
 		</div>
 	</div>
 	
