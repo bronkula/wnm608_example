c13d5d943ef7cffc6eb5854623c125144e39cee5
Hamilton <hamdiggy@gmail.com>
2020-07-21 10:22:27 -0700

    m09 html form

--- cline.hamilton/admin/users.php
+++ cline.hamilton/admin/users.php
@@ -22,21 +22,27 @@ echo <<<HTML
 		<li><a href="admin/users.php">Back</a></li>
 	</ul>
 </nav>
-<div>
-	<h2>$user->name</h2>
-	<div>
-		<strong>Type</strong>
-		<span>$user->type</span>
+<form method="post" action="">
+	<div class="form-control">
+		<label class="form-label" for="user-name">Name</label>
+		<input class="form-input" name="user-name" id="user-name" type="text" value="$user->name" placeholder="Enter the User Name">
 	</div>
-	<div>
-		<strong>Email</strong>
-		<span>$user->email</span>
+	<div class="form-control">
+		<label class="form-label" for="user-type">Type</label>
+		<input class="form-input" name="user-type" id="user-type" type="text" value="$user->type" placeholder="Enter the User Type">
 	</div>
-	<div>
-		<strong>Classes</strong>
-		<span>$classes</span>
+	<div class="form-control">
+		<label class="form-label" for="user-email">Email</label>
+		<input class="form-input" name="user-email" id="user-email" type="text" value="$user->email" placeholder="Enter the User Email">
 	</div>
-</div>
+	<div class="form-control">
+		<label class="form-label" for="user-classes">Classes</label>
+		<input class="form-input" name="user-classes" id="user-classes" type="text" value="$classes" placeholder="Enter the User Classes, comma separated">
+	</div>
+	<div class="form-control">
+		<input class="form-button" type="submit" value="Save Changes">
+	</div>
+</form>
 HTML;
 }
 
