1e988b0c9b6c30dc2d06dcb6c10a1480f8e6950d
Hamilton <hamdiggy@gmail.com>
2020-07-28 01:56:28 -0700

    m10 cart page

--- cline.hamilton/parts/templates.php
+++ cline.hamilton/parts/templates.php
@@ -16,3 +16,21 @@ return $r.<<<HTML
 </a>
 HTML;
 }
+
+
+function cartListTemplate($r,$o){
+return $r.<<<HTML
+<div class="display-flex">
+	<div class="flex-none images-thumbs">
+		<img src="/images/store/$o->thumbnail">
+	</div>
+	<div class="flex-stretch">
+		<strong>$o->title</strong>
+		<div>Delete</div>
+	</div>
+	<div class="flex-none">
+		&dollar;$o->price
+	</div>
+</div>
+HTML;
+}
--- cline.hamilton/product_cart.php
+++ cline.hamilton/product_cart.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<?php
+
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+$cart = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id` IN (4,7,10)");
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -11,10 +18,32 @@
 	<?php include "parts/navbar.php"; ?>
 
 	<div class="container">
-		<div class="card soft">
-			<h2>In Your Cart</h2>
-			<p>What do you got in there?</p>
-			<p><a href="product_checkout.php">Checkout</a></p>
+		<h2>In Your Cart</h2>
+		<div class="grid gap">
+			<div class="col-xs-12 col-md-7">
+				<div class="card soft">
+					<?= array_reduce($cart,'cartListTemplate') ?>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-5">
+				<div class="card soft flat">
+					<div class="card-section display-flex">
+						<div class="flex-stretch"><strong>Sub Total</strong></div>
+						<div class="flex-none">&dollar;3.50</div>
+					</div>
+					<div class="card-section display-flex">
+						<div class="flex-stretch"><strong>Taxes</strong></div>
+						<div class="flex-none">&dollar;3.50</div>
+					</div>
+					<div class="card-section display-flex">
+						<div class="flex-stretch"><strong>Total</strong></div>
+						<div class="flex-none">&dollar;7.00</div>
+					</div>
+					<div class="card-section">
+						<a href="product_checkout.php" class="form-button">Checkout</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	
